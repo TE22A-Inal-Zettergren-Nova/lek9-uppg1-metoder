13f3c00cc2e5375a9960e9d891e612516f7aec39
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-11-20 09:11:49 +0100

    skapat metod som ritar en linje

--- src/App.java
+++ src/App.java
@@ -4,11 +4,19 @@ public class App {
     public static void main(String[] args) throws Exception {
         double summa = omkretsCirkel(2);
         System.out.println("Omkretsen av cirkeln är " + summa);
+        ritaLinje(23);
     }
 
      static double omkretsCirkel(double radie)
     {
         double summa = 3.14*2*radie;
         return summa;
+    }
+
+    static void ritaLinje(int längd){
+        for(;längd!=0; längd--)
+        {
+            System.out.print("-");
+        }
     }
 }
