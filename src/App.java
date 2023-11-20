59d74e0e73a9a2c2c47ebbcbdb18c7326187b6e0
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-11-20 09:00:51 +0100

    metod som räknar ut cirkelns area när kallad

--- src/App.java
+++ src/App.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        double summa = omkretsCirkel(2);
+        System.out.println("Omkretsen av cirkeln är " + summa);
+    }
 
-
+     static double omkretsCirkel(double radie)
+    {
+        double summa = 3.14*2*radie;
+        return summa;
     }
 }
